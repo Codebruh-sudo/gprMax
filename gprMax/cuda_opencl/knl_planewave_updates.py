a2d6bac7ad2fd01cf8197807a6582a4c84e5f6a5
Codebruh-sudo <abhishek2122002@gmail.com>
2026-06-01 19:49:27 +0530

    Update knl_planewave_updates.py

--- gprMax/cuda_opencl/knl_planewave_updates.py
+++ gprMax/cuda_opencl/knl_planewave_updates.py
@@ -1,38 +1,15 @@
-# Copyright (C) 2015-2025: The University of Edinburgh, United Kingdom
-#                 Authors: Craig Warren, Antonis Giannopoulos,
-#                          and Abhishek Kumar
-#
-# This file is part of gprMax.
-#
-# gprMax is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# gprMax is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with gprMax.  If not, see <http://www.gnu.org/licenses/>.
 
 from string import Template
-
-# =============================================================================
+# =====================================================================
 # STANDARD (HOMOGENEOUS) MAGNETIC UPDATE
-# =============================================================================
 # Ports: updateMagneticFields() from plane_wave.pyx
-#
 # Updates the 1D DPW auxiliary grid H fields for the standard (homogeneous)
 # case. The medium is assumed uniform — scalar coefficients from the background
 # material are passed directly as kernel arguments.
-#
 # Bulk update: parallel over j in [m[3], n-m[3]]
 # PML update:  separate kernel (update_1d_magnetic_pml) over p cells
-#
 # Reference: Equation 8 of DOI: 10.1109/LAWP.2009.2016851
-# =============================================================================
+#=============================================================================
 
 update_1d_magnetic = {
     "args_cuda": Template(
